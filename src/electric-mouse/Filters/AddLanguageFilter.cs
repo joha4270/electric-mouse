c273502a25d872660923cea12c72614455217a2c
Johannes Elgaard <joha4270@gmail.com>
2016-11-22 11:19:10 +0100

    Very first version of localization services.

---
+++ src/electric-mouse/Filters/AddLanguageFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace electric_mouse.Filters
+{
+    public class AddLanguageFilter : IActionFilter
+    {
+        private readonly ILogger<AddLanguageFilter> _logger;
+        private readonly LanguageCache _cache;
+        public AddLanguageFilter(ILogger<AddLanguageFilter> logger, LanguageCache languageCache)
+        {
+            _logger = logger;
+            _cache = languageCache;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Controller controller = context.Controller as Controller;
+            if(controller != null)
+            {
+                string lang = context.HttpContext.Request.Cookies["lang"] ?? "en-uk"; //TODO: Configure default language?
+                controller.ViewBag.Strings = _cache.GetLanguage(lang);
+            }
+
+
+            
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        
+    }
+}
---
+++ src/electric-mouse/Services/LanguageCache.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+
+namespace electric_mouse
+{
+    public class LanguageCache
+    {
+        private readonly ILogger<LanguageCache> _logger;
+        private readonly Dictionary<string, Dictionary<string, string>> loadedLanguages = new Dictionary<string, Dictionary<string, string>>();
+        private readonly IHostingEnvironment _env;
+        public LanguageCache(ILogger<LanguageCache> logger, IHostingEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public Dictionary<string, string> GetLanguage(string languageName)
+        {
+            Dictionary<string, string> instance;
+            if(!loadedLanguages.TryGetValue(languageName, out instance))
+            {
+                instance = LoadLanguage(languageName);
+                loadedLanguages[languageName] = instance;
+            }
+
+            return instance;
+        }
+
+        private Dictionary<string, string> LoadLanguage(string languageName)
+        {
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            _logger.LogInformation($"Loading new language {languageName}");
+
+            string appPath = _env.ContentRootPath;
+            string languageFilePath = Path.Combine(appPath, "Lang", languageName + ".json");
+            
+            if(!File.Exists(languageFilePath))
+            {
+                throw new FileNotFoundException($"Language file not found. No language named {languageName}");
+            }
+
+
+            Dictionary<string, string> result = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(languageFilePath));
+            _logger.LogInformation($"Successfully loaded language {languageName} in {sw.ElapsedMilliseconds}ms containing {result.Count} strings");
+            return result;
+        }
+    }
+}
