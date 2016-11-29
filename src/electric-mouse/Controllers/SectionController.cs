dc832df2cffa452ba10a1bf0ed48e212eb80c6e6
nbreum15 <nicklasbreum@gmail.com>
2016-11-29 15:36:43 +0100

    section create view

---
+++ src/electric-mouse/Controllers/SectionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using electric_mouse.Data;
+using electric_mouse.Models.RouteItems;
+using electric_mouse.Models.RouteViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace electric_mouse.Controllers
+{
+    public class SectionController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public SectionController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SectionListViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.SectionName) || !string.IsNullOrEmpty(model.Hall))
+            {
+                RouteSection section = new RouteSection { Name = model.SectionName, RouteHall = _dbContext.RouteHalls.First() };
+                _dbContext.RouteSections.Add(section);
+                _dbContext.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(List), "Section");
+        }
+
+        public IActionResult List()
+        {
+            IList<RouteSection> sections = _dbContext.RouteSections.Include(s => s.RouteHall).Include(s => s.Routes).ToList();
+            SectionListViewModel model = new SectionListViewModel {Sections = sections};
+            model.Halls = _dbContext.RouteHalls.ToList();
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ src/electric-mouse/Models/RouteViewModels/SectionListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using electric_mouse.Models.RouteItems;
+
+namespace electric_mouse.Models.RouteViewModels
+{
+    public class SectionListViewModel
+    {
+        public IList<RouteSection> Sections { get; set; }
+        public IList<RouteHall> Halls { get; set; }
+        public string SectionName { get; set; }
+        public string Hall { get; set; }
+    }
+}
