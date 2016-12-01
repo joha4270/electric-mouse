659c45aa1112a9488d525128073b2585a06bbaee
Fynfner <kennetnlarsen@gmail.com>
2016-12-01 15:05:19 +0100

    Finished most user stuff
    Delete User is still missing

--- src/electric-mouse/Controllers/UserController.cs
+++ src/electric-mouse/Controllers/UserController.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using electric_mouse.Data;
 using electric_mouse.Models;
-using electric_mouse.Models.HallViewModels;
-using electric_mouse.Models.RouteItems;
 using electric_mouse.Models.UserViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace electric_mouse.Controllers
 {
+    //[Authorize(Roles = Services.RoleHandler.Admin)]
     public class UserController: Controller
     {
         private ApplicationDbContext _dbContext;
+        private UserManager<ApplicationUser> _manager;
 
-        public UserController(ApplicationDbContext dbContext)
+        public UserController(ApplicationDbContext dbContext, UserManager<ApplicationUser> manager)
         {
             _dbContext = dbContext;
+            _manager = manager;
         }
-
+        
         public IActionResult List()
         {
             IQueryable<ApplicationUser> users = _dbContext.Users;
@@ -32,10 +31,74 @@ namespace electric_mouse.Controllers
             return RedirectToAction(nameof(List), "Route");
         }
 
-        public Task<IActionResult> Promote(UserListViewModel model)
+        public async Task<IActionResult> PromoteMember(UserListViewModel model)
         {
-            var user = _dbContext.Users.First(h => h.ID == model.ID);
+            ApplicationUser admin = await _manager.GetUserAsync(User);
+            if (await _manager.IsInRoleAsync(admin, Services.RoleHandler.Admin))
+            {
+                var user = _dbContext.Users.First(h => h.Id == model.ID);
+                await _manager.AddToRoleAsync(user, "Administrator");
+                _dbContext.SaveChanges();
+            }
 
+            return RedirectToAction(nameof(List), "User");
+        }
+
+        public async Task<IActionResult> DemoteAdmin(UserListViewModel model)
+        {
+            ApplicationUser admin = await _manager.GetUserAsync(User);
+            if (await _manager.IsInRoleAsync(admin, Services.RoleHandler.Admin))
+            {
+                var user = _dbContext.Users.First(h => h.Id == model.ID);
+                await _manager.RemoveFromRoleAsync(user, "Administrator");
+                _dbContext.SaveChanges();
+                if (admin.Id == model.ID)
+                {
+                    return RedirectToAction(nameof(List), "Route");
+                }
+            }
+            return RedirectToAction(nameof(List), "User");
+        }
+
+        public async Task<IActionResult> Unban(UserListViewModel model)
+        {
+            ApplicationUser admin = await _manager.GetUserAsync(User);
+            if(await _manager.IsInRoleAsync(admin, Services.RoleHandler.Admin))
+            {
+                var user = _dbContext.Users.First(h => h.Id == model.ID);
+                await _manager.AddToRoleAsync(user, "Posting");
+                _dbContext.SaveChanges();
+            }
+            
+            return RedirectToAction(nameof(List), "User");
+        }
+
+        public async Task<IActionResult> Ban(UserListViewModel model)
+        {
+            ApplicationUser admin = await _manager.GetUserAsync(User);
+            if (await _manager.IsInRoleAsync(admin, Services.RoleHandler.Admin))
+            {
+                var user = _dbContext.Users.First(h => h.Id == model.ID);
+                await _manager.RemoveFromRoleAsync(user, "Posting");
+                _dbContext.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(List), "User");
+        }
+
+        public async Task<IActionResult> Delete(UserListViewModel model)
+        {
+            ApplicationUser admin = await _manager.GetUserAsync(User);
+            if (await _manager.IsInRoleAsync(admin, Services.RoleHandler.Admin))
+            {
+                
+                var user = _dbContext.Users.First(h => h.Id == model.ID);
+                await _manager.DeleteAsync(user);
+                _dbContext.SaveChanges();
+            }
+            
+
+            return RedirectToAction(nameof(List), "User");
         }
     }
 }
--- src/electric-mouse/Models/UserViewModels/UserListViewModel.cs
+++ src/electric-mouse/Models/UserViewModels/UserListViewModel.cs
@@ -7,9 +7,7 @@ namespace electric_mouse.Models.UserViewModels
 {
     public class UserListViewModel
     {
-        //public int State { get; set; }
         public IList<ApplicationUser> Users { get; set; }
-        public string Name { get; set; }
-        public int ID { get; set; }
+        public string ID { get; set; }
     }
 }
