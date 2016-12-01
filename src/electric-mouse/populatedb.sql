c8b24041bcef8c1d1df72623cd3d09b8aef8072e
Jakob Bo Søndergaard Madsen <jakobbsm@gmail.com>
2016-12-01 16:04:21 +0100

    Pre-commit

--- src/electric-mouse/populatedb.sql
+++
@@ -1,6 +0,0 @@
-BEGIN TRANSACTION;
-		INSERT INTO Halls (RouteHallID, Name) VALUES (1, "Boulder Hall"), (2, "Sports Hall");
-		INSERT INTO Sections (RouteSectionID, RouteHallID, Name) VALUES (1, 1, "A"), (2, 1, "B"), (3, 1, "C"), (4, 1, "D"),
-																		(5, 2, "A"), (6, 2, "B"), (7, 2, "C"), (8, 2, "D");
-		INSERT INTO Difficulties (RouteDifficultyID, Name) VALUES (1, "Green"), (2, "Blue"), (3, "Red"), (4, "Black");
-COMMIT;
