6cd792f2a75624dc60f62b9a6a470f6a81f275e5
Johannes Elgaard <joha4270@gmail.com>
2016-12-03 14:08:58 +0100

    Fetching from server, but no style and no selection. no loading of existing either

---
+++ src/electric-mouse/Models/Api/UserSearchUserResultModel.cs
@@ -0,0 +1,19 @@
+﻿namespace electric_mouse.Models.Api
+{
+    public class UserSearchUserResultModel
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string ImageUrl { get; set; }
+
+        public static UserSearchUserResultModel FromApplicationUser(ApplicationUser applicationUser)
+        {
+            return new UserSearchUserResultModel
+            {
+                ImageUrl = applicationUser.URLPath,
+                Name = applicationUser.DisplayName,
+                UserId = applicationUser.Id
+            };
+        }
+    }
+}
