f99a6b2d5a2dc40965c4c5120191e9281f0731ef
Nicklas Breum <nbreum15@student.aau.dk>
2016-12-09 18:14:03 +0100

    copied from other branch

---
+++ test/ExampleTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace test
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void Add_TwoPlusTwo_Returns4() 
+        {
+            // Arrange
+            int a = 2;
+            int b = 2;
+            int expected = 4;
+
+            // Act
+            int actual = Add(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        private int Add(int a, int b) => a + b;
+    }
+}
