d5ce59c719bac006da4def35416742ddb1911b3a
Johannes Elgaard <joha4270@gmail.com>
2016-12-09 14:36:16 +0100

    Initial test project

---
+++ test/electric-mouse-tests/SampleTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace electric_mouse_tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
