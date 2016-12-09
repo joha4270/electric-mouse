7907f3e1a0d2bc8a438da15d15447127dd5ac513
Nicklas Breum <nbreum15@student.aau.dk>
2016-12-09 17:52:39 +0100

    setup xunit, moq and reference to main project

---
+++ test/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using electric_mouse.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
